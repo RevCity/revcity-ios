bc3afae19892f80dbb718303285fc4ab51256e8d
Jma353 <jma353@cornell.edu>
2017-03-20 13:00:49 -0400

    More tab bar developments

--- RevCity/TabBarController.swift
+++ RevCity/TabBarController.swift
@@ -15,7 +15,6 @@ class TabBarController: UIViewController {
     var numberOfTabs: Int = 0
     var tabBarContainerView: UIView = UIView()
     var tabBarButtons: [UIButton] = [UIButton]()
-    var transparentBarEnabled: Bool = false
     var tabBarButtonFireEvent: UIControlEvents = .touchDown
     var tabBarIsHidden: Bool = false
     var preselectedTabIndex = 0
@@ -59,19 +58,51 @@ class TabBarController: UIViewController {
     
     /** Create the container for the tab bar **/
     func createTabBarContainerView() {
-        // TODO
+        /* Container view aesthetics */
+        tabBarContainerView = UIView(frame: CGRect(x: 0, y: view.frame.height - tabBarHeight, width: view.frame.width, height: tabBarHeight))
+        tabBarContainerView.backgroundColor = tabBarColor
+        
+        // TODO - more setup
+        
+        view.addSubview(tabBarContainerView)
+        
     }
     
     
     /** Setup the tabs **/
     func setupTabs() {
-        // TODO
+        
+        /* Clear just in case */
+        tabBarButtons.removeAll()
+        
+        /* Overall styling info for each tab */
+        let tabBarButtonWidth = view.frame.width / CGFloat(numberOfTabs)
+        var xOffset: CGFloat = 0.0
+        
+        /* Make all tabbar buttons */
+        for i in 0 ..< numberOfTabs {
+            /* Style */
+            let tabBarButton = UIButton(frame: CGRect(x: xOffset, y: 0, width: tabBarButtonWidth, height: tabBarHeight))
+            tabBarButton.backgroundColor = .clear
+            tabBarButton.setImage(selectedBarButtonImages[i], for: .selected)
+            tabBarButton.setImage(unSelectedBarButtonImages[i], for: .normal)
+            
+            /* Add target */
+            // TODO - set target
+            
+            /* Add to view / list */
+            tabBarContainerView.addSubview(tabBarButton)
+            tabBarButtons.append(tabBarButton)
+            
+            /* Increment for appropriate spacing */
+            xOffset += tabBarButtonWidth
+        }
     }
     
     
     /** Automation of the pressing of a button at a particular index **/
     func programmaticallyPressTabBarButton(atIndex index: Int) {
-        // TODO 
+        // TODO
     }
     
     
